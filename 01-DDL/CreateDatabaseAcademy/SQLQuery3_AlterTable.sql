7d7a0e3243c90ffe04e8422ff0bc03780757a782
alkeity <alkeity.23@gmail.com>
2024-10-18 13:35:03 +0300

    classwork: DML

---
+++ 01-DDL/CreateDatabaseAcademy/SQLQuery3_AlterTable.sql
@@ -0,0 +1,4 @@
+--USE PD311_AcademyDesign;
+
+--ALTER TABLE Teachers
+--	ALTER COLUMN teacher_id INT PRIMARY KEY IDENTITY(1,1)
---
+++ 01-DDL/DML/SQLQuery1_InsertTeachers.sql
@@ -0,0 +1,9 @@
+USE PD311_AcademyDesign;
+GO
+
+INSERT Teachers
+(last_name, first_name, middle_name, birth_date, year_started, rate)
+VALUES
+('Волосова', 'Кристина', 'Станиславовна', '2001-01-04', '2024-10-16', 35),
+('Колесова', 'Александра', 'Евгеньевна', '1995-12-23', '2024-10-16', 35)
+
